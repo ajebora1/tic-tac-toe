dc708d986f9805ef761339df51e57225df6b154b
ajebora1 <rachel_ajebon@yahoo.com>
2022-06-03 03:47:44 -0400

    Added event listener

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-/*----- constants -----*/
+const displayStatus = document.querySelector('h2');
+const players = {
+    '1': 'X',
+    '-1': 'O',
+ }
 
-/*----- app's state (variables) -----*/
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 1;
+let isGameActive = true;
+let gameWinner = {}
+//let playerTurn = {}
 
-/*----- cached element references -----*/
 
-/*----- event listeners -----*/
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-/*----- functions -----*/
+function initGame() {
+  //  let currentPlayer = 1;  
+}
+  
+
+$("div").click(function(evt){
+    if(board[evt.target.id] === '') {
+        board[evt.target.id] = currentPlayer
+        evt.target.innerText = players[currentPlayer]
+        currentPlayer *= -1
+       // document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+       } else {
+        console.log("not working")
+    }
+  });
+
+//   $('#square div').each(function(idx) {
+//     console.log(this.innerText);
+//   });
+//render()
+
+function changePlayer() {
+ 
+}
+
+function validateResults() {
+
+}
+
+function restartGame() {
+ 
+}
